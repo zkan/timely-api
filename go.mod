1f960a5773b3073188908e05a648bdf825fa4f6d
zkan <zkan.cs@gmail.com>
2020-05-04 15:02:48 +0700

    :art: Fix code style using fmt :bear:

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	github.com/gorilla/mux v1.7.4
 	github.com/lib/pq v1.4.0
-	github.com/spf13/viper v1.6.3 // indirect
+	github.com/spf13/viper v1.6.3
 )
--- main.go
+++ main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-   "database/sql"
-   "fmt"
-   "net/http"
-   "log"
-   "time"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
-   "github.com/zkan/timely-api/internal/middleware"
-   "github.com/zkan/timely-api/task"
-   "github.com/gorilla/mux"
-    _ "github.com/lib/pq"
-   "github.com/spf13/viper"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+	"github.com/zkan/timely-api/internal/middleware"
+	"github.com/zkan/timely-api/task"
 )
 
 func init() {
-   viper.SetDefault("port", "1323")
-   viper.SetDefault("cors.allow_origin", "*")
-   viper.SetDefault("db.conn.string", "host=localhost user=postgres password=mysecretpassword dbname=postgres sslmode=disable")
+	viper.SetDefault("port", "1323")
+	viper.SetDefault("cors.allow_origin", "*")
+	viper.SetDefault("db.conn.string", "host=localhost user=postgres password=mysecretpassword dbname=postgres sslmode=disable")
 }
 
 func newDBClient(connStr string) (*sql.DB, error) {
-   return sql.Open("postgres", connStr)
+	return sql.Open("postgres", connStr)
 }
 
 func main() {
-   r := mux.NewRouter()
-   r.Use(mux.CORSMethodMiddleware(r))
-   r.Use(middleware.Headers(viper.GetString("cors.allow_origin")))
+	r := mux.NewRouter()
+	r.Use(mux.CORSMethodMiddleware(r))
+	r.Use(middleware.Headers(viper.GetString("cors.allow_origin")))
 
-   r.HandleFunc("/healths", func(w http.ResponseWriter, r *http.Request) {
-       fmt.Fprint(w, "ok")
-   })
+	r.HandleFunc("/healths", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "ok")
+	})
 
-   db, err := newDBClient(viper.GetString("db.conn.string"))
-   if err != nil {
-       log.Fatal(err)
-   }
-   defer db.Close()
+	db, err := newDBClient(viper.GetString("db.conn.string"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-   r.HandleFunc("/tasks", task.List(db)).Methods(http.MethodGet)
+	r.HandleFunc("/tasks", task.List(db)).Methods(http.MethodGet)
 
-   srv := &http.Server{
-      Handler:      r,
-      Addr:         "0.0.0.0:" + viper.GetString("port"),
-      WriteTimeout: 15 * time.Second,
-      ReadTimeout:  15 * time.Second,
-   }
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:" + viper.GetString("port"),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
 
-   srv.ListenAndServe()
+	srv.ListenAndServe()
 }

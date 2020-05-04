15cb6ffc6b222213a7c8fa7492cb567e83b67632
zkan <zkan.cs@gmail.com>
2020-05-04 14:33:21 +0700

    :card_file_box: Remodel db so we will have only one table for tasks :bear:

--- db/migrations/000001_create_categories_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS "public"."categories" (
-  "id" SERIAL NOT NULL,
-  "name" varchar(30),
-  PRIMARY KEY ("id")
-);
--- db/migrations/000001_create_tasks_table.up.sql
+++ db/migrations/000001_create_tasks_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS "public"."tasks" (
     "id" SERIAL NOT NULL,
-    "category_id" int4,
     "name" varchar(100),
-    "started_at" timestamp,
-    "ended_at" timestamp,
+    "category" varchar(100),
+    "start" timestamp,
+    "end" timestamp,
     PRIMARY KEY ("id")
 );

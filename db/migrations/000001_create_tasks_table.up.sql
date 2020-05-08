d828745b436632c7d8423eff175720df4e7fbdde
zkan <zkan.cs@gmail.com>
2020-05-08 20:05:53 +0700

    :card_file_box: Add author field :bear:

--- db/migrations/000001_create_tasks_table.up.sql
+++ db/migrations/000001_create_tasks_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS "public"."tasks" (
     "id" SERIAL NOT NULL,
     "name" varchar(100),
     "category" varchar(100),
+    "author" varchar(100),
     "started_at" timestamp,
     "ended_at" timestamp,
     PRIMARY KEY ("id")
